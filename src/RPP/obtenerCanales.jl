946f62ae7e28cab30b4ef8e8885b19f835b2d490
RPP <rperdomo396@gmail.com>
2022-10-26 21:40:04 -0300

    ahora obtenerCanales() levanta el comtrade

--- src/RPP/obtenerCanales.jl
+++ src/RPP/obtenerCanales.jl
@@ -1,6 +1,15 @@
 #funcion para obtener canales
-using Gtk
-using COMTRADE
-pathCOMTRADE = open_dialog_native("Elegir COMTRADE .CFG", GtkNullContainer(), String[])
-#sacar el ".cfg" o el ".DAT" del path obtenido para pasarle al read_comtrade
-z1 = read_comtrade()
+    using Gtk
+    using COMTRADE
+function obtenerCanales()
+
+    pathCOMTRADE = open_dialog_native("Elegir COMTRADE .CFG", GtkNullContainer(), String[])
+   
+    #sacar el ".cfg" o el ".DAT" del path obtenido para pasarle al read_comtrade
+    pathCOMTRADE = replace(pathCOMTRADE, ".DAT" =>"")
+    pathCOMTRADE = replace(path, ".dat" =>"")
+    pathCOMTRADE = replace(path, ".CFG" =>"")
+    pathCOMTRADE = replace(path, ".cfg" =>"")
+    return(read_comtrade(pathCOMTRADE))
+    
+end
