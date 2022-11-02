e74a1e7113d6286d66af140d0ecd484a47f551f7
DGM1971 <dgm1971@gmail.com>
2022-11-02 01:57:21 -0300

    bosquejando estructuras de datos

--- src/estructurasDatos.jl
+++ src/estructurasDatos.jl
@@ -1,9 +1,25 @@
+struct Tensiones
+
+"""
+    Canal
+
+    Contiene la informacion sampleada representada como muestras a una determinada frecuncia de muestreo. 
+    El valor está expresado en magnitudes físicas primarias.
+"""
 struct Canal
     id::Int #El lugar donde se ubica en el comtrade
     nombre::String #nombre del canal: vr,vs,vt,ir,is,it
     valores_instantaneos::Any #vector de reales (valores instantáneos)
     #valores_fasor::Any #vector de complejos (valores RMS)
     #frecuencia_muestreo::Any #en Hz cantidad de muestras por segundos
+    #a multiplicador del canal
+    #b ofset del canal
+    #uu unidades del canal
+
+    # The channel conversion factor is ax+b. The stored data value of x, in the data (.DAT) file, correspond to a sampled value of (ax+b) in units (uu) specified above. The rules of mathematical parsing are followed such that the data sample “x” is multiplied by the gain factor “a” and then the offset factor “b” is added. Manipulation of the data value by the 
+    # conversion factor restores the original sampled values. See Annex E for an example.
+ # al leerlos del archivo ya deberían quedar en primarios y con la cte correspondiente aplicada para llevarlo a magnitudes reales
+
 end
 function obtenerCanales(pathCOMTRADE, [id])#pathDataFrame: la ruta del comtrade; id el un vector de
     #retorna los  6 u 9 canales de corrientes y tensiones
