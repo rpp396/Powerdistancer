29e9f92fb646c3f1c336fd34531866fc9d995358
RPP <rperdomo396@gmail.com>
2022-11-23 11:29:33 -0300

    Test de leer_canales

--- src/RPP/canales.jl
+++ src/RPP/canales.jl
@@ -5,46 +5,46 @@
 	El valor está expresado en magnitudes físicas primarias.
 """
 struct Canal
-	valores::Vector #vector de reales (valores instantáneos)
-	unidad::String #Volt, Amper, Hz, etc...
-	frecuencia_muestreo::Any #en Hz cantidad de muestras por segundos
+    valores::Vector #vector de reales (valores instantáneos)
+    unidad::String #Volt, Amper, Hz, etc...
+    frecuencia_muestreo::Any #en Hz cantidad de muestras por segundos
 
 end
 
 abstract type Sistema_trifasico end
 
 struct Sistema_trifasico_fasores <: Sistema_trifasico
-	tiempo_de_muestra::Vector
-	va::Canal
-	vb::Canal
-	vc::Canal
-	ia::Canal
-	ib::Canal
-	ic::Canal
-	frecuencia::Number
-	frecuencia_muestreo::Number #en Hz cantidad de muestras por segundos
+    tiempo_de_muestra::Vector
+    va::Canal
+    vb::Canal
+    vc::Canal
+    ia::Canal
+    ib::Canal
+    ic::Canal
+    frecuencia::Number
+    frecuencia_muestreo::Number #en Hz cantidad de muestras por segundos
 end
 
 struct Sistema_trifasico_RMS <: Sistema_trifasico
-	tiempo_de_muestra::Vector
-	va::Canal
-	vb::Canal
-	vc::Canal
-	ia::Canal
-	ib::Canal
-	ic::Canal
-	frecuencia::Number
-	frecuencia_muestreo::Number #en Hz cantidad de muestras por segundos
+    tiempo_de_muestra::Vector
+    va::Canal
+    vb::Canal
+    vc::Canal
+    ia::Canal
+    ib::Canal
+    ic::Canal
+    frecuencia::Number
+    frecuencia_muestreo::Number #en Hz cantidad de muestras por segundos
 end
 
 struct Sistema_trifasico_instanteneos <: Sistema_trifasico
-	tiempo_de_muestra::Vector
-	va::Canal
-	vb::Canal
-	vc::Canal
-	ia::Canal
-	ib::Canal
-	ic::Canal
-	frecuencia::Number
-	frecuencia_muestreo::Number #en Hz cantidad de muestras por segundos
+    tiempo_de_muestra::Vector
+    va::Canal
+    vb::Canal
+    vc::Canal
+    ia::Canal
+    ib::Canal
+    ic::Canal
+    frecuencia::Number
+    frecuencia_muestreo::Number #en Hz cantidad de muestras por segundos
 end
--- test/leerCanalesTest.jl
+++ test/leerCanalesTest.jl
@@ -1,9 +1,9 @@
-ruta = joinpath("data", "Comtrade", "osc01p")
 
-prueba = leer_canales(path=ruta)
 
 # pruebas de los algoritmos de cálculo de frecuencia
 @testset verbose = true "Prueba de lectura de COMTRADE y pasaje a Sistema_trifasico_instanteneos" begin
-    @test length(prueba.va) = 4000
+    ruta = joinpath("data", "Comtrade", "osc01p")
+    prueba = leer_canales(path=ruta)
+    @test length(prueba.va.valores) == 4000
 
 end
