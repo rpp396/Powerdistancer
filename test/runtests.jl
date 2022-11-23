06f9db375716dd970aa1cff7676b26cc720541cc
RPP <rperdomo396@gmail.com>
2022-11-23 11:11:07 -0300

    Limpiar ejemplos marcelo en test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,20 +2,21 @@ using Test, Powerdistancer
 
 #include("lectura.jl")
 
-@testset "Funciones Rodrigo" begin
-	# include("arithmetic.jl")
-	# include("utils.jl")
+@testset verbose = true "Funciones Rodrigo" begin
+    include("leerCanalesTest.jl")
+    # include("arithmetic.jl")
+    # include("utils.jl")
 end
 
 @testset verbose = true "Funciones Daniel" begin
-	include("frecuencia.jl")
-	include("rms.jl")
-	# include("utils.jl")
+    include("frecuencia.jl")
+    include("rms.jl")
+    # include("utils.jl")
 end
 
 @testset "Funciones generales" begin
-	# include("arithmetic.jl")
-	# include("utils.jl")
+    # include("arithmetic.jl")
+    # include("utils.jl")
 end
 
 #@test Powerdistancer.f() == 1
