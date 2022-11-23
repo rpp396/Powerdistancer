9b9c9b927652a64e3ed85f93a680cc270a66c64f
RPP <rperdomo396@gmail.com>
2022-11-23 13:26:24 -0300

    Pasar datos numericos a tipo Float64

--- src/RPP/canales.jl
+++ src/RPP/canales.jl
@@ -5,46 +5,46 @@
 	El valor está expresado en magnitudes físicas primarias.
 """
 struct Canal
-    valores::Vector #vector de reales (valores instantáneos)
+    valores::Vector{Float64} #vector de reales (valores instantáneos)
     unidad::String #Volt, Amper, Hz, etc...
-    frecuencia_muestreo::Any #en Hz cantidad de muestras por segundos
+    frecuencia_muestreo::Float64 #en Hz cantidad de muestras por segundos
 
 end
 
 abstract type Sistema_trifasico end
 
 struct Sistema_trifasico_fasores <: Sistema_trifasico
-    tiempo_de_muestra::Vector
+    tiempo_de_muestra::Vector{Float64}
     va::Canal
     vb::Canal
     vc::Canal
     ia::Canal
     ib::Canal
     ic::Canal
-    frecuencia::Number
-    frecuencia_muestreo::Number #en Hz cantidad de muestras por segundos
+    frecuencia::Float64
+    frecuencia_muestreo::Float64 #en Hz cantidad de muestras por segundos
 end
 
 struct Sistema_trifasico_RMS <: Sistema_trifasico
-    tiempo_de_muestra::Vector
+    tiempo_de_muestra::Vector{Float64}
     va::Canal
     vb::Canal
     vc::Canal
     ia::Canal
     ib::Canal
     ic::Canal
-    frecuencia::Number
-    frecuencia_muestreo::Number #en Hz cantidad de muestras por segundos
+    frecuencia::Float64
+    frecuencia_muestreo::Float64 #en Hz cantidad de muestras por segundos
 end
 
 struct Sistema_trifasico_instanteneos <: Sistema_trifasico
-    tiempo_de_muestra::Vector
+    tiempo_de_muestra::Vector{Float64}
     va::Canal
     vb::Canal
     vc::Canal
     ia::Canal
     ib::Canal
     ic::Canal
-    frecuencia::Number
-    frecuencia_muestreo::Number #en Hz cantidad de muestras por segundos
+    frecuencia::Float64
+    frecuencia_muestreo::Float64 #en Hz cantidad de muestras por segundos
 end
--- src/RPP/comtrade.jl
+++ src/RPP/comtrade.jl
@@ -7,44 +7,44 @@ En el caso de contar con sumas de corrientes (interruptor y medio), se pasa el p
 ejemplo_3_corrientes = Powerdistancer.leer_canales(path=ruta, conf=[1, 2, 3, 4, 5, 6])
 ejemplo_3_corr aientes = Powerdistancer.leer_canales(path=ruta, conf=[1, 2, 3, 4, 5, 6],i2=true, i2conf=[4, 5, 6])
 """
-function leer_canales(; path, conf = [1, 2, 3, 4, 5, 6], i2 = false, i2conf = [7, 8, 9], debug = false)
-	comtrade = read_comtrade(path)
-	#Nota: el comando read_comtrade ya toma en cuenta el factor de escalamiento "a" y de offset "b"
+function leer_canales(; path, conf=[1, 2, 3, 4, 5, 6], i2=false, i2conf=[7, 8, 9], debug=false)
+    comtrade = read_comtrade(path)
+    #Nota: el comando read_comtrade ya toma en cuenta el factor de escalamiento "a" y de offset "b"
 
-	#= 
-					comandos utiles
-					VSCodeServer.vscodedisplay(comtrade.cfg.A) #Para ver los DataFrames
-					VSCodeServer.vscodedisplay(comtrade.cfg.D) #Para ver los DataFrames
-					VSCodeServer.vscodedisplay(comtrade.dat) #Para ver los DataFrames 
-					=#
-	frecuencia_muestreo = comtrade.cfg.samp[1]
-	tiempo_de_muestra = comtrade.dat[!, 2]
-	va = Canal(comtrade.dat[!, conf[1]+2], comtrade.cfg.A[conf[1], 5], frecuencia_muestreo)
-	vb = Canal(comtrade.dat[!, conf[2]+2], comtrade.cfg.A[conf[2], 5], frecuencia_muestreo)
-	vc = Canal(comtrade.dat[!, conf[3]+2], comtrade.cfg.A[conf[3], 5], frecuencia_muestreo)
-	if i2
-		ia = Canal(comtrade.dat[!, conf[4]+2] + comtrade.dat[!, i2conf[1]+2], comtrade.cfg.A[conf[4], 5], frecuencia_muestreo)
-		ib = Canal(comtrade.dat[!, conf[5]+2] + comtrade.dat[!, i2conf[2]+2], comtrade.cfg.A[conf[5], 5], frecuencia_muestreo)
-		ic = Canal(comtrade.dat[!, conf[6]+2] + comtrade.dat[!, i2conf[3]+2], comtrade.cfg.A[conf[6], 5], frecuencia_muestreo)
-	else
-		ia = Canal(comtrade.dat[!, conf[4]+2], comtrade.cfg.A[conf[4], 5], frecuencia_muestreo)
-		ib = Canal(comtrade.dat[!, conf[5]+2], comtrade.cfg.A[conf[5], 5], frecuencia_muestreo)
-		ic = Canal(comtrade.dat[!, conf[6]+2], comtrade.cfg.A[conf[6], 5], frecuencia_muestreo)
-	end
-	#usar algorirmo de frecuencia para calcular
-	#el tiempo en que se tomó cada muestra, empezando la muesta n = 1 en tiempo = 0 
-	frecuencia = estimate_frequency(va.valores, Int64(round(5 * frecuencia_muestreo / 50)), Int64(frecuencia_muestreo))
+    #= 
+    					comandos utiles
+    					VSCodeServer.vscodedisplay(comtrade.cfg.A) #Para ver los DataFrames
+    					VSCodeServer.vscodedisplay(comtrade.cfg.D) #Para ver los DataFrames
+    					VSCodeServer.vscodedisplay(comtrade.dat) #Para ver los DataFrames 
+    					=#
+    frecuencia_muestreo = comtrade.cfg.samp[1]
+    tiempo_de_muestra = comtrade.dat[!, 2]
+    va = Canal(comtrade.dat[!, conf[1]+2], comtrade.cfg.A[conf[1], 5], frecuencia_muestreo)
+    vb = Canal(comtrade.dat[!, conf[2]+2], comtrade.cfg.A[conf[2], 5], frecuencia_muestreo)
+    vc = Canal(comtrade.dat[!, conf[3]+2], comtrade.cfg.A[conf[3], 5], frecuencia_muestreo)
+    if i2
+        ia = Canal(comtrade.dat[!, conf[4]+2] + comtrade.dat[!, i2conf[1]+2], comtrade.cfg.A[conf[4], 5], frecuencia_muestreo)
+        ib = Canal(comtrade.dat[!, conf[5]+2] + comtrade.dat[!, i2conf[2]+2], comtrade.cfg.A[conf[5], 5], frecuencia_muestreo)
+        ic = Canal(comtrade.dat[!, conf[6]+2] + comtrade.dat[!, i2conf[3]+2], comtrade.cfg.A[conf[6], 5], frecuencia_muestreo)
+    else
+        ia = Canal(comtrade.dat[!, conf[4]+2], comtrade.cfg.A[conf[4], 5], frecuencia_muestreo)
+        ib = Canal(comtrade.dat[!, conf[5]+2], comtrade.cfg.A[conf[5], 5], frecuencia_muestreo)
+        ic = Canal(comtrade.dat[!, conf[6]+2], comtrade.cfg.A[conf[6], 5], frecuencia_muestreo)
+    end
+    #usar algorirmo de frecuencia para calcular
+    #el tiempo en que se tomó cada muestra, empezando la muesta n = 1 en tiempo = 0 
+    frecuencia = estimate_frequency(va.valores, Int64(round(5 * frecuencia_muestreo / 50)), Int64(frecuencia_muestreo))
 
-	if debug
-		println("nrates: $(comtrade.cfg.nrates)")
-		println("samp: $(comtrade.cfg.samp)")
-		println("endsamp: $(comtrade.cfg.endsamp)")
-		println("frecuencia de muestreo $(frecuencia_muestreo)")
-		println("triggertime: $(comtrade.cfg.triggertime)")
-		println("time: $(comtrade.cfg.time)")
-		#println("triggertime-time: $(comtrade.cfg.triggertime - comtrade.cfg.time)")
-	end
-	return Sistema_trifasico_instanteneos(tiempo_de_muestra, va, vb, vc, ia, ib, ic, frecuencia, frecuencia_muestreo)
+    if debug
+        println("nrates: $(comtrade.cfg.nrates)")
+        println("samp: $(comtrade.cfg.samp)")
+        println("endsamp: $(comtrade.cfg.endsamp)")
+        println("frecuencia de muestreo $(frecuencia_muestreo)")
+        println("triggertime: $(comtrade.cfg.triggertime)")
+        println("time: $(comtrade.cfg.time)")
+        #println("triggertime-time: $(comtrade.cfg.triggertime - comtrade.cfg.time)")
+    end
+    return Sistema_trifasico_instanteneos(tiempo_de_muestra, va, vb, vc, ia, ib, ic, frecuencia, frecuencia_muestreo)
 end
 
 
