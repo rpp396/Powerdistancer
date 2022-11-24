94ad50263122e97b9667422fe2bcbc743a710d8e
RPP <rperdomo396@gmail.com>
2022-11-24 14:12:23 -0300

    fasores1

--- src/RPP/fasores.jl
+++ src/RPP/fasores.jl
@@ -1,11 +1,10 @@
 using Powerdistancer
 ruta = joinpath("data", "Comtrade", "osc04p") #falta AN
 sistema = leer_canales(path=ruta)
-señal = sistema.ia.valores
 f = sistema.frecuencia
 fs = sistema.frecuencia_muestreo
 t0 = 0
-tmax = ((length(señal)) / fs) - 1 / fs
+tmax = ((length(sistema.ia.valores)) / fs) - 1 / fs
 t = 0.02:1/fs:0.04
 FFT = Powerdistancer.fft(señal[1:100])
 F = Powerdistancer.fftshift(FFT)
@@ -26,28 +25,30 @@ function generarSeno(; duracion=5, f=50, fs=10000, valorPico=1)
 
     v = Vector{Float64}()
     for i = 1:(duracion*fs)
-        push!(v, valorPico * sin(f * 2 * pi * i / (fs)))
+        push!(v, valorPico * sin(f * 2 * pi * (i-1) / (fs)))
     end
     return v
 end
 
-seno = generarSeno(duracion=0.8, f=50, fs=5000, valorPico=1000)
-FFT = Powerdistancer.fft(seno[101:200])
+seno = generarSeno(duracion=0.8, f=52, fs=5000, valorPico=1)
+FFT = Powerdistancer.fft(seno[1:100])
+Powerdistancer.fftfreq(1000, 10)
+Powerdistancer.fftfreq(1000, 10)[anchoVentana]
 F = Powerdistancer.fftshift(FFT)
-Powerdistancer.fftfreq(length(1:100), 5000)
-FFT[2]
-FFT = Powerdistancer.fft(señal[1:100])[2]
-FFT = Powerdistancer.fft(señal[101:200])[2]
-FFT = Powerdistancer.fft(señal[201:300])[2]
-FFT = Powerdistancer.fft(señal[301:400])[2]
-FFT = Powerdistancer.fft(señal[401:500])[2]
-FFT = Powerdistancer.fft(señal[501:600])[2]
-FFT = Powerdistancer.fft(señal[600:701])[2]
-for i = 1:100:3500
+anchoVentana=100
+for i = 1:anchoVentana:3500
     #=    println(i)
        println(i+99)
      =#
     @show i
+    @show i+anchoVentana-1
     println("tiempo: $(i/5000)")
-    @show FFT = Powerdistancer.fft(seno[i:(i+200)])[2]
+    @show anchoVentana = 100
+    @show FFT = Powerdistancer.fft(seno[i:(i+anchoVentana-1)])[2]/(anchoVentana/2)
+    @show anchoVentana = 200
+    @show FFT = Powerdistancer.fft(seno[i:(i+anchoVentana-1)])[3]/(anchoVentana/2)
 end
+
+anchoVentana = 10
+fs = 5000.0
+Powerdistancer.fftfreq(anchoVentana, fs)[trunc(Int,anchoVentana/2)-1:1:trunc(Int,anchoVentana/2)+1]
